c4e0b2b7b894fc971691543349410d4b85cb610e
Agustín <daniel.babarzi@gmail.com>
2021-04-09 17:10:18 -0300

    iconos

--- menu_gestion.php
+++ menu_gestion.php
@@ -2,17 +2,17 @@
 	<!-- SIDEBAR !-->
 	<div class="bg-gray-200 w-56">
 		<!-- ROL !-->
-		<img src="imagenes/gestor.svg" class="w-28 flex m-auto mt-4">
+		<img src="imagenes/gestor.svg" class="w-20 flex m-auto mt-4">
 		<!-- NAV !-->
 		<nav class="flex flex-col items-center h-screen">
 			<div class="flex flex-col">
 					<div class="flex-1 group cursor-help relative">
-								 <img src="imagenes/pregunta.svg" class="w-28 flex m-auto mt-4">
+								 <img src="imagenes/pregunta.svg" class="w-20 flex m-auto mt-4">
 										<div class="opacity-0 w-28 bg-blue-100 text-grey text-center
 										text-xs rounded-lg py-2 absolute z-10 group-hover:opacity-100 bottom-full
 										-right-1/1 ml-14 px-3 pointer-events-none lg:-mt-8">
 
-										En el perfil de enfermeros/as se puede ver la información vinculada a los ciudadanos en los vacunatorios en los que usted trabaja
+										<i>En el perfil de enfermeros/as se puede ver la información vinculada a los ciudadanos en los vacunatorios en los que usted trabaja</i>
 											</svg>
 										</div>
 									</div>
@@ -22,7 +22,7 @@
 				<button class="py-2 5 px-4 m-auto mt-4 hover:bg-gray-400">Boton 3</button>
 			</div>
 			<div class="mt-auto">
-				<button class="py-2 5 px-4 m-auto mt-4 hover:bg-gray-400" name="boton" value="logout" formnovalidate=""><img src="imagenes/cerrar.png" class="w-16 m-auto"></button>
+				<button class="w-28 py-2 5 px-4 m-auto mt-4 hover:bg-gray-400" name="boton" value="logout" formnovalidate=""><img src="imagenes/cerrar.png" class="w-16 m-auto"></button>
 			</div>
 		</nav>
 	</div>

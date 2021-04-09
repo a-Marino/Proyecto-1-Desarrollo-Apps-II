b161ea191e6e456ce2057877b159b91650474bda
a-Marino <andresmarinolaiolo@hotmail.com.ar>
2021-04-09 17:43:53 -0300

    Se agrego el tooltip en todos los menus y varios cambios mas

--- menu_gestion.php
+++ menu_gestion.php
@@ -6,22 +6,20 @@
 		<!-- NAV !-->
 		<nav class="flex flex-col items-center h-screen">
 			<div class="flex flex-col">
-					<div class="flex-1 group cursor-help relative">
-						<img src="imagenes/pregunta.png" class="w-20 flex m-auto mt-4">
-							<div class="opacity-0 w-28 bg-blue-100 text-grey text-center
-							text-xs rounded-lg py-2 absolute z-10 group-hover:opacity-100 bottom-full
-							-right-1/1 ml-14 px-3 pointer-events-none lg:-mt-8">
 
-							En el perfil de enfermeros/as se puede ver la información vinculada a los ciudadanos en los vacunatorios en los que usted trabaja
-									
-							</div>
-					</div>
-
-				<button class="py-2 5 px-4 m-auto mt-4 hover:bg-gray-400">Boton 1</button>
-				<button class="py-2 5 px-4 m-auto mt-4 hover:bg-gray-400">Boton 2</button>
-				<button class="py-2 5 px-4 m-auto mt-4 hover:bg-gray-400">Boton 3</button>
+				<button class="py-2 5 px-4 m-auto mt-4 hover:bg-gray-400" formnovalidate>Boton 1</button>
+				<button class="py-2 5 px-4 m-auto mt-4 hover:bg-gray-400" formnovalidate>Boton 2</button>
+				<button class="py-2 5 px-4 m-auto mt-4 hover:bg-gray-400" formnovalidate>Boton 3</button>
 			</div>
 			<div class="mt-auto">
+				<div class="flex-1 group cursor-help relative">
+					<img src="imagenes/pregunta.png" class="w-16 flex m-auto mt-4">
+						<div class="opacity-0 w-28 bg-blue-100 text-grey text-center
+						text-xs rounded-lg py-2 absolute z-10 group-hover:opacity-100 bottom-full
+						-right-1/1 ml-14 px-3 pointer-events-none lg:-mt-8">
+							En el perfil de gestores/as se puede ver la información vinculada a los ciudadanos en los vacunatorios.		
+						</div>
+				</div>
 				<button class="py-2 5 px-4 m-auto mt-4 hover:bg-gray-400" name="boton" value="logout" formnovalidate=""><img src="imagenes/cerrar.png" class="w-16 m-auto"></button>
 			</div>
 		</nav>

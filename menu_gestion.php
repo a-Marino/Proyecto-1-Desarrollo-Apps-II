b1334ba0f9e953efbc2b4c488913a7c23115ee5b
Agustín <daniel.babarzi@gmail.com>
2021-04-09 15:08:46 -0300

    tooltip

--- menu_gestion.php
+++ menu_gestion.php
@@ -2,10 +2,21 @@
 	<!-- SIDEBAR !-->
 	<div class="bg-gray-200 w-56">
 		<!-- ROL !-->
-		<img src="imagenes/gestor.png" class="w-28 flex m-auto mt-4">
+		<img src="imagenes/gestor.svg" class="w-28 flex m-auto mt-4">
 		<!-- NAV !-->
 		<nav class="flex flex-col items-center h-screen">
 			<div class="flex flex-col">
+					<div class="flex-1 group cursor-help relative">
+								 <img src="imagenes/pregunta.svg" class="w-28 flex m-auto mt-4">
+										<div class="opacity-0 w-28 bg-blue-100 text-grey text-center
+										text-xs rounded-lg py-2 absolute z-10 group-hover:opacity-100 bottom-full
+										-right-1/1 ml-14 px-3 pointer-events-none lg:-mt-8">
+
+										En el perfil de enfermeros/as se puede ver la información vinculada a los ciudadanos en los vacunatorios en los que usted trabaja
+											</svg>
+										</div>
+									</div>
+
 				<button class="py-2 5 px-4 m-auto mt-4 hover:bg-gray-400">Boton 1</button>
 				<button class="py-2 5 px-4 m-auto mt-4 hover:bg-gray-400">Boton 2</button>
 				<button class="py-2 5 px-4 m-auto mt-4 hover:bg-gray-400">Boton 3</button>
@@ -21,7 +32,7 @@
 		<div class="border-2 border-blue-600 rounded-xl shadow-lg p-4 ml-10 mr-10">
 			<h2 class="text-2xl text-center"><?php echo $apelnom ?><h2>
 		</div>
-		<?php 
+		<?php
 			// SWITCH CON BOTONES
 		?>
 	</div>

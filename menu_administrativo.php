e4197ee51fa4e226a9a0b7b5ffec6ece8227f189
a-Marino <andresmarinolaiolo@hotmail.com.ar>
2021-04-14 14:58:36 -0300

    ABM-Usuarios

--- menu_administrativo.php
+++ menu_administrativo.php
@@ -34,6 +34,20 @@
 					case 'asignaciones':
 						require_once 'asignaciones.php';
 						break;
+					case 'graba_usuario':
+						$dni=$_POST['dni'];
+						$nomApell= htmlentities(addslashes($_POST['nomApell']));
+						$telefono=$_POST['telefono'];
+						$role=$_POST['rol'];
+						$rup=$_POST['rup'];
+						$clave=$_POST['clave'];
+						
+						$sql= "INSERT INTO usuarios(DNI,apelnom,telefono,role,RUP,clave)VALUES($dni,'$nomApell',$telefono,'$role',$rup,'$clave')";
+							
+						$resultado = $conexion->query($sql);
+
+						$boton='abm-usuario';
+
 					case 'abm-usuario':
 						require_once 'abm-usuario.php';
 						break;

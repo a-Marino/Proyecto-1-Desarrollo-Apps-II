da25570492b6ad943df3baf930e6a1a6b42b875f
Agustín Daniel <daniel.babarzi@gmail.com>
2021-04-19 15:16:29 -0300

    gif offline

--- error_inicio.php
+++ error_inicio.php
@@ -3,4 +3,4 @@ margin-top: -150px;
 margin-left: -215px;
 left: 50%;
 top: 40%;
-position: absolute;" src="imagenes/offline.png" alt="Offline">
+position: absolute;" src="imagenes/offline.gif" alt="Offline">
